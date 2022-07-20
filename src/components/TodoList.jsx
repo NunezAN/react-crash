cf7fadad0c7670861924884006214c2a0fd8cc12
NunezAN <alex101nunez@gmail.com>
2022-07-19 21:37:26 -0500

    initial styling for to-do list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,27 @@
-import "./TodoList.css"
+import "./TodoList.css";
 
-function TodoList(){ 
-    const taskOne = "Wash Clothes"
-    return(
-        <ul>
-            <li className="checkbox__item">
-                <input type="checkbox" id="checkbox1"/>
-                <label htmlFor="checkbox1">{`${taskOne}`}</label>
-            </li>
-            <li>
-                <input type="checkbox" id="checkbox2"/>
-                <label htmlFor="checkbox2">Get Grocries</label>
-            </li>
-            <li>
-                <input type="checkbox" id="checkbox3"/>
-                <label htmlFor="checkbox3">Make Matcha</label>
-            </li>
-        </ul>
-    );
+function TodoList() {
+  const taskOne = "Wash Clothes";
+  const taskTwo = "Finish React";
+  const taskThree = "Drink Matcha";
+  return (
+    <div className="container">
+      <div className="TodoList--container">
+        <div className="TodoItem">
+          <input type="checkbox" id="checkbox1" />
+          <label className="Todo__Label" htmlFor="checkbox1">{`${taskOne}`}</label>
+        </div>
+        <div className="TodoItem">
+          <input type="checkbox" id="checkbox2" />
+          <label className="Todo__Label" htmlFor="checkbox2">{`${taskTwo}`}</label>
+        </div>
+        <div className="TodoItem">
+          <input type="checkbox" id="checkbox3" />
+          <label className="Todo__Label" htmlFor="checkbox3">{`${taskThree}`}</label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TodoList;
