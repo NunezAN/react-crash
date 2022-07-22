5820b4db06b9bad0caa7973e1c2a6656966b54ec
NunezAN <alex101nunez@gmail.com>
2022-07-22 08:39:58 -0500

    remvoing check box warning to todo app

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,7 +4,7 @@ function TodoList(props) {
   return (
     <div>
         <div className="TodoItem">
-          <input type="checkbox" id="checkbox" checked={props.item.completed}/>
+          <input type="checkbox" id="checkbox" checked={props.item.completed} onChange={()=> console.log("changed")}/>
           <label className="Todo__Labe" htmlFor="checkbox">{props.item.text}</label>
         </div>
     </div>
