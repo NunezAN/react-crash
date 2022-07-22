d56787f57eee9d06f53a0e20b1a58514a22b55c6
NunezAN <alex101nunez@gmail.com>
2022-07-21 20:11:12 -0500

    using contructor to populate state into jsx object

--- src/App.jsx
+++ src/App.jsx
@@ -27,71 +27,40 @@ import React from "react";
 //   )
 // }
 
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       answer: "Yes",
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         hello
+//       </div>
+//     );
+//   }
+// }
+
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {
-      answer:"Yes"
+    this.obj = {
+      name: "Alex",
+      age: 26,
     };
   }
-
+  
   render() {
+    console.log(this.obj.age);
     return (
       <div>
-        <Header username={this.state.answer}></Header>
-        <Greeting></Greeting>
+        <h1>{this.obj.name}</h1>
+        <h3>{this.obj.age} years old</h3>
       </div>
     );
   }
 }
-//#2
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <p>Welcome,{this.props.username}</p>
-      </header>
-    );
-  }
-}
-// function Header(props) {
-//   console.log(props.username);
-//   return (
-//     <header>
-//       <p>Welcome,{props.username}</p>
-//     </header>
-//   );
-// }
-//#3
-// function Greeting() {
-//   const date = new Date();
-//   const hours = date.getHours();
-//   let timeOfDay;
-//   if (hours < 12) {
-//     timeOfDay = "morning";
-//   } else if (hours >= 12 && hours < 17) {
-//     timeOfDay = "afternoon";
-//   } else {
-//     timeOfDay = "night";
-//   }
-
-//   return <h1>Good {timeOfDay} to you, sir ir madem!</h1>;
-// }
-class Greeting extends React.Component {
-  render() {
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay;
-    if (hours < 12) {
-      timeOfDay = "morning";
-    } else if (hours >= 12 && hours < 17) {
-      timeOfDay = "afternoon";
-    } else {
-      timeOfDay = "night";
-    }
-
-    return <h1>Good {timeOfDay} to you, sir ir madem!</h1>;
-  }
-}
-
 export default App;
