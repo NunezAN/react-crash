829cae8fac19c1cb504461bafe3eb386cab1aabe
NunezAN <alex101nunez@gmail.com>
2022-07-23 09:30:29 -0500

    dynamically change state using bind and set state

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,57 @@ import "./App.css";
 import todosData from "./components/todosData.js";
 import React from "react";
 
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       data: todosData};
+//   }
+
+//   render() {
+//     const todosComponent = this.state.data.map((todoitem) => {
+//       return <TodoItem key={todoitem.id} item={todoitem}></TodoItem>;
+//     });
+//     console.log(this.state);
+//     return (
+//       <div className="container">
+//         <div className="TodoList--container">{todosComponent}</div>
+//       </div>
+//     );
+//   }
+// }
+
 class App extends React.Component {
   constructor() {
     super();
-    this.state = todosData;
+    this.state = {
+      count: 1,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClick__half = this.handleClick__half.bind(this);
   }
 
-  render() {
-    const todosComponent = this.state.map((todoitem) => {
-      return <TodoItem key={todoitem.id} item={todoitem}></TodoItem>;
+  handleClick(){
+    this.setState(prevState=> {
+      return {
+        count : prevState.count *2
+      }
     });
-    console.log(this.state);
+  }
+
+  handleClick__half(){
+    this.setState(prevState=>{
+      return{
+        count: prevState.count / 2 
+      }
+    })
+  }
+  render() {
     return (
-      <div className="container">
-        <div className="TodoList--container">{todosComponent}</div>
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change-double!</button>
+        <button onClick={this.handleClick__half}>Change!-half</button>
       </div>
     );
   }
