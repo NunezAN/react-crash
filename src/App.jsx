4a50d44cc250c6117735e474da5982b39401fc6a
NunezAN <alex101nunez@gmail.com>
2022-07-25 19:40:49 -0500

    added styling to completed task - todo app

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,40 @@ import todosData from "./components/todosData.js";
 import React from "react";
 import Conditional from "./components/conditional.jsx";
 
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       data: todosData};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: todosData
+    };
+      this.handleChange = this.handleChange.bind(this);
+  }
 
-//       this.handleChange = this.handleChange.bind(this);
-//   }
+  handleChange(id){
+    this.setState(prevState =>{
+      const newState = prevState.data.map(obj => {
+        if(obj.id == id){
+          obj.completed = !obj.completed;
+        }
+        return obj;
+      })
+      return{data: newState};
+    })
+  }
 
-//   handleChange(id){
-//     this.setState(prevState =>{
-//       const newState = prevState.data.map(obj => {
-//         if(obj.id == id){
-//           obj.completed = !obj.completed;
-//         }
-//         return obj;
-//       })
-//       return{data: newState};
-//     })
-//   }
+  render() {
+    const todosComponent = this.state.data.map((todoitem) => {
+      return <TodoItem key={todoitem.id} item={todoitem} handleChange={this.handleChange}></TodoItem>;
+    });
+    
+    return (
+      <div className="container">
+        <div className="TodoList--container">{todosComponent}</div>
+      </div>
+    );
+  }
 
-//   render() {
-//     const todosComponent = this.state.data.map((todoitem) => {
-//       return <TodoItem key={todoitem.id} item={todoitem} handleChange={this.handleChange}></TodoItem>;
-//     });
-//     return (
-//       <div className="container">
-//         <div className="TodoList--container">{todosComponent}</div>
-//       </div>
-//     );
-//   }
-
-// }
+}
 
 // class App extends React.Component {
 //   constructor() {
@@ -63,31 +64,31 @@ import Conditional from "./components/conditional.jsx";
 //   }
 // }
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      loggedIn: true,
-    };
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       loggedIn: true,
+//     };
 
-    this.logClick = this.logClick.bind(this);
-  }
+//     this.logClick = this.logClick.bind(this);
+//   }
 
-  logClick() {
-    this.setState(prevState=>{
-      return {loggedIn: !prevState.loggedIn}
-    });
-  }
+//   logClick() {
+//     this.setState(prevState=>{
+//       return {loggedIn: !prevState.loggedIn}
+//     });
+//   }
 
-  render() {
-    return (
-      <div>
-        <div>{this.state.loggedIn ? "Logged In" : "Logged Out"}</div>
-        <button onClick={this.logClick}>
-          {this.state.loggedIn ? "Log Out" : "log In"}
-        </button>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <div>{this.state.loggedIn ? "Logged In" : "Logged Out"}</div>
+//         <button onClick={this.logClick}>
+//           {this.state.loggedIn ? "Log Out" : "log In"}
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 export default App;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,14 @@
 import "./TodoItem.css";
 
 function TodoList(props) {
+  let classCompleted = props.item.completed ? "completed" : "notCompleted";
+  console.log(`${props.item.id}${classCompleted}`);
   return (
     <div>
         <div className="TodoItem">
-          <input type="checkbox" id="checkbox" checked={props.item.completed} onChange={()=> props.handleChange(props.item.id)}/>
-          <label className="Todo__Label" htmlFor="checkbox">{props.item.text}</label>
+          <input type="checkbox" id="checkbox" checked={props.item.completed}
+          onChange={()=> props.handleChange(props.item.id)}/>
+          <label className={`Todo__Label ${classCompleted}`} htmlFor="checkbox">{props.item.text}</label>
         </div>
     </div>
   );
